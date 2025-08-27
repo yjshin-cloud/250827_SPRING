4e4bc1b5c8488a071c823e6d99e10f5192c12b82
yjshin.cloud <yjshin.cloud@gmail.com>
2025-08-27 10:22:21 +0900

    Initial commit

---
+++ src/main/java/org/example/SpringCore.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SpringCore {
+    public static void main(String[] args) {
+        System.out.println("----- 스프링 컨테이너 초기화 시작 -----");
+    }
+
+    // context ? -> 앱 설정. AppConfig? SpringConfig?
+    // Bean...
+}
